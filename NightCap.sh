9bdb4e1d8f24b54e24777459976c66ecabdf280b
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-06-19 11:26:11 +0800

    Update NightCap.sh

--- NightCap.sh
+++ NightCap.sh
@@ -2,6 +2,6 @@
 while :
 do
   DATE=$(date +"%Y%m%d_%H%M%S")
-  raspistill -ISO 800 -ss 8000000 -o /home/stargazer/AstroPhoto\ Plus/Sequences/Photos/$DATE.jpg -q 100 &
+  raspistill -ISO 800 -ss 6500000 -o /home/aspi/AllSkyData/$DATE.jpg -q 100 &
 sleep 60
 done
