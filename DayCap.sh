901717a6afb48389f3a6a4694ba5264ddea5cfab
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-06-19 11:25:36 +0800

    Update DayCap.sh

--- DayCap.sh
+++ DayCap.sh
@@ -2,6 +2,6 @@
 while :
 do
   DATE=$(date +"%Y%m%d_%H%M%S")
-  raspistill -o /home/stargazer/AstroPhoto\ Plus/Sequences/Photos/$DATE.jpg &
+  raspistill -o /home/aspi/AllSkyData/$DATE.jpg &
 sleep 60
 done
