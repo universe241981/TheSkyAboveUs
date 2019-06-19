95f65185f12aab5cea44846449a4d5e93b2a15d0
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-06-19 12:07:52 +0800

    Create display_latest_image.php
    Can be rename to index.php

---
+++ display_latest_image.php
@@ -0,0 +1,26 @@
+<?php
+$page = $_SERVER['PHP_SELF'];
+?>
+<html>
+<head>
+<meta http-equiv="refresh" content="60" URL='<?php echo $page?>'">
+<title>The Sky Above Us</title>
+</head>
+    <body bgcolor=black>
+        <center><font size=8 color=white>The Sky Above Us</font><br><font size=5 color=white>Location = Somewhere On Earth | Owner = W.C.Observatory | </font>
+        <font size=5 color=white>
+        <?php
+
+            $files = glob('AllSkyData/*.*');
+            $files = array_combine($files, array_map('filectime', $files));
+            arsort($files);
+            echo "<br><a href='" . key($files) . "' target='_blank'><img src='" . key($files) . "' height=100%></a>";
+
+        ?>
+        
+        <br><font size=3 color=white>Powered by <a href="https://www.raspberrypi.org/" target="_blank"><font color=white>Raspberry Pi</a> and <a href="https://sites.google.com/site/meteotuxpi/home" target="_blank"><font color=white>Meteotux PI</a>.</font>
+        <center><font size=3 color=white>Integrated by William Chin</font>
+        
+
+    </body>
+</html>
