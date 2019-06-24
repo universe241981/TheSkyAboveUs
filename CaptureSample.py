494e7c4286ecd83779a29a029a89d2a46e4ea029
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-06-24 11:03:19 +0800

    Update CaptureSample.py

--- CaptureSample.py
+++ CaptureSample.py
@@ -5,8 +5,7 @@ from picamera import PiCamera
 import time
 from time import sleep
 camera = PiCamera()
-while True:
+camera.resolution = (2592, 1944)
     DATE = (time.strftime("%y%m%d_%H:%M:%S"))
     variable = "/home/aspi/AllSkyData/" + DATE + ".jpg"
     camera.capture(variable)
-    sleep(5)
