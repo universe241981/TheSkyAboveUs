6335740d4ccd52fb8ed14145ec8a2ee9c886488d
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-06-24 12:24:11 +0800

    Update CaptureSample.py

--- CaptureSample.py
+++ CaptureSample.py
@@ -10,3 +10,15 @@ sleep(2)
 DATE = (time.strftime("%y%m%d_%H%M%S"))
 variable = "/home/aspi/AllSkyData/" + DATE + ".jpg"
 camera.capture(variable)
+
+
+from time import sleep
+from picamera import PiCamera
+from datetime import datetime
+
+camera = PiCamera()
+camera.start_preview()
+wait()
+for filename in camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M}.jpg'):
+    print('Captured %s' % filename)
+    wait()
