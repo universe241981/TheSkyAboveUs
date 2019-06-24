8e0702600cd3149b0d41a77b9f322247d0137510
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-06-24 15:16:48 +0800

    Update CaptureSample.py

--- CaptureSample.py
+++ CaptureSample.py
@@ -37,3 +37,5 @@ while True:
     DATE = (time.strftime("%Y%m%d_%H%M%S"))
     camera.capture("/home/aspi/AllSkyData/" + DATE + ".jpg")
     sleep(30)
+
+    # 640×480, 800×600, 960×720, 1024×768, 1280×960, 1400×1050, 1440×1080, 1600×1200, 1856×1392, 1920×1440, and 2048×1536
