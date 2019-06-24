78fb1a334d83ea3c0fc528c41332466d5973fdee
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-06-24 10:32:12 +0800

    Update CaptureSample.py

--- CaptureSample.py
+++ CaptureSample.py
@@ -1,3 +1,6 @@
+#For Installation of Picamera for enable python control of Pi camera 
+#https://picamera.readthedocs.io/en/release-1.13/install.html
+
 from picamera import PiCamera
 import time
 from time import sleep
