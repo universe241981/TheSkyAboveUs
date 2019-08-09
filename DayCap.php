36d1e2af5642764cc87fcd68e6ad9996daadfb0c
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-08-09 16:31:45 +0800

    Create DayCap.php

---
+++ DayCap.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>The Sky Above Us - Daytime Capture Mode</title>
+</head>
+<body>
+    <?php
+    exec('/usr/bin/python2.7 /aspi/AllSkyData/DayCap.py');
+    ?>
+</body>
+</html>
