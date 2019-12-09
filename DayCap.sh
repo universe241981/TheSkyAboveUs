0a0476faac5751dcc2a0899c887415170583fca9
universe241981 <47937972+universe241981@users.noreply.github.com>
2019-12-10 00:01:05 +0800

    Update DayCap.sh

--- DayCap.sh
+++ DayCap.sh
@@ -2,10 +2,11 @@
 while :
 do
   DATE=$(date --utc +"%Y%m%d-%H%M%S%Z")
-  raspistill -o /home/aspi/AllSkyData/$DATE.jpg -mm spot -br 45 &
+  raspistill -ISO 100 -ss 200 -o /home/aspi/AllSkyData/$DATE.jpg -q 100 &
 sleep 60
 done
 
+#raspistill -o /home/aspi/AllSkyData/$DATE.jpg -mm spot -br 45 & being mod on 10/12/2019
 #For 8MP Pi camera v2
 #raspistill -mm backlit -drc high -o $DATE.jpg -w 1640 -h 1232 &
 
