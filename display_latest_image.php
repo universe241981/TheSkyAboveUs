59e6d3151553147ee19247cca6a78f3bf5d5818a
universe241981 <47937972+universe241981@users.noreply.github.com>
2020-02-18 10:03:49 +0800

    Update display_latest_image.php

--- display_latest_image.php
+++ display_latest_image.php
@@ -16,14 +16,14 @@ $page = $_SERVER['PHP_SELF'];
         ?> 
         </font>
 
-        <br><font size=5 color=white>
-        <?php
-            $f = fopen("/sys/class/thermal/thermal_zone0/temp","r");
-            $temp = fgets($f);
-            echo 'SoC Temp. = '.round($temp/1000). '&#8451;</font>';
-            fclose($f);
-        ?>
-        </font>
+#        <br><font size=5 color=white>
+#        <?php
+#            $f = fopen("/sys/class/thermal/thermal_zone0/temp","r");
+#            $temp = fgets($f);
+#            echo 'SoC Temp. = '.round($temp/1000). '&#8451;</font>';
+#            fclose($f);
+#        ?>
+#        </font>
                                                                  
         <?php
 
